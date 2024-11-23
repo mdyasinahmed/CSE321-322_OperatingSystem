f0f866820758cc280e7246d0f8275d4c22b98cf7
Md. Yasin Ahmed Mahi <121636177+mdyasinahmed@users.noreply.github.com>
2024-11-23 20:47:13 +0600

    add File Handling Simulation in Shell Script.sh

---
+++ basic_operations/File Handling Simulation in Shell Script.sh
@@ -0,0 +1,17 @@
+# Create a new file
+echo "Creating a file named example.txt"
+touch example.txt
+
+# Write to the file
+echo "Hello, Operating Systems Lab!" > example.txt
+echo "Written to example.txt"
+
+# Read the file
+echo "Contents of example.txt:"
+cat example.txt
+
+# Delete the file
+echo "Deleting example.txt"
+rm example.txt
+echo "File deleted successfully"
+
